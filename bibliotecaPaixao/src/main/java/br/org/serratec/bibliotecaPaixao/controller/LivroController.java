b480d0aa37348c851c5b49257eab9d97990e8557
mateus zeze <mateuszebendovp30@gmail.com>
2024-05-14 17:00:03 -0300

    feat: adiciona classes referentes ao livro

---
+++ bibliotecaPaixao/src/main/java/br/org/serratec/bibliotecaPaixao/controller/LivroController.java
@@ -0,0 +1,47 @@
+package br.org.serratec.bibliotecaPaixao.controller;
+
+import br.org.serratec.bibliotecaPaixao.entities.Livro;
+import br.org.serratec.bibliotecaPaixao.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/livros")
+public class LivroController {
+
+    @Autowired
+    LivroService livroService;
+
+    @PostMapping
+    public ResponseEntity<Object> save (@RequestBody Livro livro){
+        return ResponseEntity.status(HttpStatus.CREATED).body(livroService.save(livro));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Livro>> findAll () {
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.findAll());
+    }
+
+    @GetMapping ("/{id}")
+    public ResponseEntity<Livro> find (@PathVariable Integer id) {
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.findById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Livro> update (@RequestBody Livro livro){
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.update(livro));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Object> delete (@RequestBody Livro livro){
+        var livro0 = livroService.findById(livro.getLivroId());
+        if(livroService.delete(livro) != null) return ResponseEntity.status(HttpStatus.OK).body("{'Status': 'Deletado com sucesso', " +
+                livro0.toString() +
+                "}");
+        return ResponseEntity.status(HttpStatus.OK).body("{'Status': 'Deletado com sucesso' }");
+    }
+}
---
+++ bibliotecaPaixao/src/main/java/br/org/serratec/bibliotecaPaixao/entities/Livro.java
@@ -0,0 +1,106 @@
+package br.org.serratec.bibliotecaPaixao.entities;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name="livro")
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "livroId",
+        scope = Livro.class
+)
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="livro_id")
+    private Integer livroId;
+
+    @Column(name="nome_livro")
+    private String nomeLivro;
+
+    @Column(name="nome_autor")
+    private String nomeAutor;
+
+    @Column(name="data_lancamento")
+    private LocalDate dataLancamento;
+
+    @Column(name="codigo_isbn")
+    private String codigoIsbn;
+
+    @ManyToOne
+    @JoinColumn(name="editora_id")
+    private Editora editora;
+
+    @OneToMany
+    private List<Emprestimo> emprestimos;
+
+    public Integer getLivroId() {
+        return livroId;
+    }
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getCodigoIsbn() {
+        return codigoIsbn;
+    }
+
+    public void setCodigoIsbn(String codigoIsbn) {
+        this.codigoIsbn = codigoIsbn;
+    }
+
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public void setEditora(Editora editora) {
+        this.editora = editora;
+    }
+
+    public List<Emprestimo> getEmprestimos() {
+        return emprestimos;
+    }
+
+    public void setEmprestimos(List<Emprestimo> emprestimos) {
+        this.emprestimos = emprestimos;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "livroId=" + livroId +
+                ", nomeLivro='" + nomeLivro + '\'' +
+                ", nomeAutor='" + nomeAutor + '\'' +
+                ", dataLancamento=" + dataLancamento +
+                ", codigoIsbn='" + codigoIsbn + '\'' +
+                ", editora=" + editora +
+                ", emprestimos=" + emprestimos +
+                '}';
+    }
+}
